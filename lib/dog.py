422c8ce24b8a72a5ea7ddc19602f602302be0865
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-12-14 01:42:48 -0500

    Uploading changes

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,33 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name='Joe', breed='Pug'):
+        self.name = name
+        if breed not in APPROVED_BREEDS:
+            print('Breed must be in list of approved breeds.')
+        else:
+            self.breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 < len(name) < 25:
+            self._name = name
+        else:
+            print('Name must be string between 1 and 25 characters.')
+        pass
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, breed):
+        if breed not in APPROVED_BREEDS:
+            print('Breed must be in list of approved breeds.')
+        else:
+            self._breed = breed
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,21 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = "Joe", job = "Admin" ):
+        self.name = name
+        if job not in APPROVED_JOBS:
+            print('Job must be in list of approved jobs.')
+        else:
+            self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 < len(name) < 25:
+            poo = name.title()
+            self._name = poo
+        else:
+            print('Name must be string between 1 and 25 characters.')
